bcf56fc40328fa1b008496fe33428608ee4f3972
Kali Donovan <kali@deviantech.com>
2013-11-30 12:06:45 -0800

    More direct approach, hooking into caching flow to rename automatically

--- lib/carrierwave-mimetype-fu.rb
+++ lib/carrierwave-mimetype-fu.rb
@@ -5,7 +5,7 @@ module CarrierWave
     extend ActiveSupport::Concern
 
     included do
-      alias_method_chain :current_path, :mimetype_fu_ext
+      alias_method_chain :cache!, :mimetype_fu_magic
       
       begin
         require 'mimetype_fu'
@@ -15,160 +15,27 @@ module CarrierWave
       end
     end
 
-    module ClassMethods
-      def set_mimetype_fu_content_type(override=false)
-        process :set_mimetype_fu_content_type => override
+    def cache_with_mimetype_fu_magic!(new_file = sanitized_file)
+      # Only step in on the initial file upload
+      return cache_without_mimetype_fu_magic!(new_file) unless new_file.is_a?(ActionDispatch::Http::UploadedFile)
+      
+      begin
+        # Collect information about the real content type
+        real_content_type = File.mime_type?( File.open(new_file.path) ).split(';').first
+        valid_extensions  = Array(MIME::Types[real_content_type].try(:first).try(:extensions))
+        
+        # Set proper content type, and update filename if current name doesn't match reach content type
+        new_file.content_type = real_content_type
+        new_file  = CarrierWave::SanitizedFile.new(new_file)
+        base, ext = new_file.send(:split_extension, new_file.original_filename)
+        ext = valid_extensions.first unless valid_extensions.include?(ext)
+        
+        new_file.instance_variable_set '@original_filename', [base, ext].join('.')
+      rescue StandardError => e
+        Rails.logger.warn "[carrierwave-mimetype-fu] Exception raised, not fixing image extension. #{e}" 
+      ensure
+        cache_without_mimetype_fu_magic!(new_file)
       end
-    end
-
-    # List from https://github.com/carrierwaveuploader/carrierwave/pull/949
-    MIMETYPE_FU_GENERIC_CONTENT_TYPES = %w[      
-      application-x/octet-stream
-
-      application/
-      application/*
-      application/binary
-      application/download
-      application/download-file
-      application/downloadfile
-      application/force-download
-      application/force_download
-      application/oclet-stream
-      application/octec-stream
-      application/octect-stream
-      application/octed-stream
-      application/octet
-      application/octet-binary
-      application/octet-stream
-      application/octet_stream
-      application/octetstream
-      application/octlet-stream
-      application/save
-      application/save-as
-      application/stream
-      application/unknown
-      application/x-download
-      application/x-file-download
-      application/x-force-download
-      application/x-forcedownload
-      application/x-octet-stream
-      application/x-octets
-      application/x-octetstream
-      application/x-unknown
-      application/x-unknown-application-octet-stream
-      application/x-unknown-content-type
-      application/x-unknown-octet-stream
-
-      applicaton/octet-stream
-
-      attachment/octet-stream
-
-      bad/type
-
-      binary/
-      binary/*
-      binary/octec-stream
-      binary/octet-stream
-      binary/octet_stream
-      binary/octetstream
-
-      content-transfer-encoding/binary
-
-      download/file
-      download/test
-
-      file/octet-stream
-      file/unknown
-
-      multipart/alternative
-      multipart/form-data
-      multipart/octet-stream
-
-      octet/stream
-
-      type/unknown
-
-      unknown/
-      unknown/application
-      unknown/data
-      unknown/unknown
-
-      x-application/octet-stream
-      x-application/octetstream
-
-      x-unknown/octet-stream
-      x-unknown/x-unknown
-    ]
-
-    def generic_content_type_for_mimetype_fu?(content_type)
-      MIMETYPE_FU_GENERIC_CONTENT_TYPES.include? content_type
-    end
-
-
-
-    def mimetype_fu_ext
-      MIME::Types[file.content_type].try(:first).try(:extensions).try(:first) || file.extension
-    end
-    
-    def filename_with_mimetype_fu_ext
-      file.send(:split_extension, file.original_filename)[0] + '.' + mimetype_fu_ext
-    end
-    
-    def current_path_with_mimetype_fu_ext
-      File.join(File.dirname(current_path_without_mimetype_fu_ext), filename_with_mimetype_fu_ext)
-    end
-
-    # Move cached file under new name so naive methods down the line will guess content type correctly
-    def rename_file_with_proper_extension
-      Rails.logger.fatal "[rename_file_with_proper_extension] File.basename(current_path_without_mimetype_fu_ext) != filename_with_mimetype_fu_ext: #{File.basename(current_path_without_mimetype_fu_ext) != filename_with_mimetype_fu_ext}"
-      if File.basename(current_path_without_mimetype_fu_ext) != filename_with_mimetype_fu_ext
-        Rails.logger.fatal "[rename_file_with_proper_extension] File.exists?(current_path_without_mimetype_fu_ext) && !File.exists?(current_path_with_mimetype_fu_ext): #{File.exists?(current_path) && !File.exists?(current_path_with_mimetype_fu_ext)}"
-        if File.exists?(current_path_without_mimetype_fu_ext) && !File.exists?(current_path_with_mimetype_fu_ext)
-          Rails.logger.fatal "[rename_file_with_proper_extension] MOVING FILE!"
-          FileUtils.mv(current_path_without_mimetype_fu_ext, current_path_with_mimetype_fu_ext)
-        end
-      end 
-    end
-    
-
-    ##
-    # Changes the file content_type using the mimetype-fu gem
-    #
-    def set_mimetype_fu_content_type(override=false)
-      if override || file.content_type.blank? || generic_content_type_for_mimetype_fu?(file.content_type)
-        # cache_stored_file! unless cached! # Allows use with remote, previously-uploaded files (e.g. collecting meta on existing files)
-        cache! unless File.exists?(file.path) # Allows use with remote, previously-uploaded files (e.g. collecting meta on existing files)        
-        
-        new_content_type = File.mime_type?( File.open(current_path_without_mimetype_fu_ext) ).split(';').first
-
-        # TODO - REMOVE LOGGING STATEMENTS
-        Rails.logger.fatal "[set_mimetype_fu_content_type] #{current_path} should be #{new_content_type}"
-
-        if file.respond_to?(:content_type=)
-          file.content_type = new_content_type
-        else
-          file.instance_variable_set(:@content_type, new_content_type)
-        end
-        
-        # Now move the file to the new path
-        rename_file_with_proper_extension
-
-        # Overrwrite the file var with a new one based on the new proper file extension
-        file = CarrierWave::SanitizedFile.new(current_path_with_mimetype_fu_ext)
-        if file.respond_to?(:content_type=)
-          file.content_type = new_content_type
-        else
-          file.instance_variable_set(:@content_type, new_content_type)
-        end
-        
-        # Update the instance to act as if the file had been originally uploaded with the proper extension
-        current_path = current_path_with_mimetype_fu_ext
-        instance_variable_set '@file',              file
-        instance_variable_set '@filename',          filename_with_mimetype_fu_ext
-        instance_variable_set '@original_filename', filename_with_mimetype_fu_ext
-      end
-    rescue ::Exception => e
-      raise CarrierWave::ProcessingError, I18n.translate(:"errors.messages.mimetype_fu_processing_error", e: e, default: 'Failed to process file with MimetypeFu. Original Error: %{e}')
     end
 
   end
